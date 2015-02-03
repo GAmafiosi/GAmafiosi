58f77061a3df618b996dca7c564dfd0bba7b66fe
anegg0 <gael.blanchemain@gmail.com>
2015-02-03 09:08:15 -0500

    + session post

--- soundcloud/config/routes.rb
+++ soundcloud/config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
   get '/users' => 'user#index'
+  get '/session' => 'session#new'
+  post '/session' => 'session#create'
   resources :users do
   resources :songs
   end
